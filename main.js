877099ae4ae29a463cb9ca5adbfaaceb460cd416
dorotaszot <62651157+dorotaszot@users.noreply.github.com>
2020-06-23 08:40:48 +0200

    done

---
+++ main.js
@@ -0,0 +1,38 @@
+// Initialize and add the map
+function initMap() {
+  // Your location
+  const loc = { lat: 51.507351, lng: -0.127758 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
+// Sticky menu background opacity as we scroll down
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(e) {
+  if (this.hash !== '') {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
